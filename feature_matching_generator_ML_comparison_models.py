4aee9f746af6dfbf05c35316881c28e01223002d
alex <alexandrosrotsidis@icloud.com>
2022-09-29 10:30:27 +0300

    wip

--- feature_matching_generator_ML_comparison_models.py
+++ feature_matching_generator_ML_comparison_models.py
@@ -135,6 +135,9 @@ def feature_matcher_wrapper_generic_comparison_model(base_path, comparison_data_
 
         save_debug_image(image_gt_path, keypoints_xy, predicted_keypoint_xy, debug_images_path, query_image)
 
+        import pdb
+        pdb.set_trace()
+
         # from now on I will be using the descs and keypoints that Predicting Matchability (2014) / MatchNoMatch 2020 deemed matchable
         queryDescriptors = queryDescriptors[predictions == 1]  # replacing queryDescriptors here so to keep code changes minimal
         keypoints_xy = keypoints_xy[predictions == 1]  # replacing keypoints_xy as they are mapped to queryDescriptors
