f7474825092bfca18127aeb34864e3aa8db18c48
alex <alexandrosrotsidis@icloud.com>
2022-09-29 17:12:16 +0300

    wip

--- create_training_data_match_no_match.py
+++ create_training_data_match_no_match.py
@@ -78,7 +78,7 @@ def get_subset_of_pairs(all_pair_ids, no):
         pair_id = all_pair_ids[i][0]
         img_id_1, img_id_2 = pair_id_to_image_ids(pair_id)
         if((img_id_1 not in img_left_ids) and (img_id_2 not in img_right_ids)):
-            pair_ids.append(pair_id)
+            pair_ids.append(all_pair_ids[i]) #which is a tuple
     return pair_ids
 
 def createDataForMatchNoMatchMatchabilityComparison(image_base_dir, image_live_dir, db_live, live_images, output_db_path, use_all_pairs=True):
