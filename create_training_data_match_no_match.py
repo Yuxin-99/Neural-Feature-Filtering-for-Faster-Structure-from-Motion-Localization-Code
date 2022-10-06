55cd9db14f595209246e36cf073ffda207f64969
alex <alexandrosrotsidis@icloud.com>
2022-10-06 17:19:43 +0300

    wip

--- create_training_data_match_no_match.py
+++ create_training_data_match_no_match.py
@@ -109,7 +109,7 @@ empty_points_3D_txt_file(points_3D_file_txt_path)
 arrange_images_txt_file(images_file_txt_path)
 
 if(model == 'live' or model == 'gt'):
-    for filename in glob.glob(f'{images_path}, session_*/*.jpg'):
+    for filename in glob.glob(images_path):
         breakpoint()
     colmap.vocab_tree_matcher(db_path)
 else:
